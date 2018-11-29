a6c78112d35c2f93a6ad0d3bec5553eeed19ec91
Jay Marcyes <jay@marcyes.com>
2018-11-29 01:12:59 -0700

    first commit, broke this out from a private project, it's based on the prom.query.Query.reduce() code but generalized so I can use for some medium data things I'm working on

---
+++ mister_test.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, division, print_function, absolute_import
+import os
+import math
+from collections import Counter
+import re
+
+import testdata
+from testdata import TestCase
+
+from mister import Mister, BaseMister
+
+
+class MrWordCount(BaseMister):
+    def prepare(self, count, path):
+        size = os.path.getsize(path)
+        length = int(math.ceil(size / count))
+        start = 0
+        for x in range(count):
+            kwargs = {}
+            kwargs["path"] = path
+            kwargs["start"] = start
+            kwargs["length"] = length
+            start += length
+            yield (), kwargs
+
+    def map(self, path, start, length):
+        output = Counter()
+        with open(path) as fp:
+            fp.seek(start, 0)
+            words = fp.read(length)
+
+        for word in re.split(r"\s+", words):
+            output[word] += 1
+        return output
+
+    def reduce(self, output, count):
+        if not output:
+            output = Counter()
+        output.update(count)
+        return output
+
+
+class MrCount(BaseMister):
+    def prepare(self, count, numbers):
+        chunk = int(math.ceil(len(numbers) / count))
+        # this splits the numbers into chunk size chunks
+        # https://stackoverflow.com/a/312464/5006
+        for i in range(0, len(numbers), chunk):
+            yield (), {"numbers": numbers[i:i + chunk]}
+
+    def map(self, numbers):
+        return sum(numbers)
+
+    def reduce(self, output, total):
+        if not output:
+            output = 0
+        output += total
+        return output
+
+
+class MrTest(TestCase):
+    def test_count(self):
+        numbers = list(range(0, 1000000))
+        total_sync = sum(numbers)
+
+        m = MrCount(numbers)
+        total_async = m.run()
+
+        self.assertEqual(total_sync, total_async)
+
+    def test_wordcount(self):
+        path = testdata.get_contents("bible-kjv").path
+        output_sync = Counter()
+        with open(path) as fp:
+            words = fp.read()
+        for word in re.split(r"\s+", words):
+            output_sync[word] += 1
+
+        mr = MrWordCount(path)
+        output_async = mr.run()
+
+        # sigh, not accounting for wordbreak bit me, turns out one of the breaks
+        # was "and" and so the counts don't line up, so we'll just test the
+        # words
+        self.assertEqual(
+            [k[0] for k in output_sync.most_common(10)],
+            [k[0] for k in output_async.most_common(10)]
+        )
+
